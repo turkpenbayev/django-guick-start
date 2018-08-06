09b9ed49113545545de8d819d88a30fa385342da
turkpenbayev <mr.turkpenbaev1997@gmail.com>
2018-08-06 21:32:27 +0600

    added serch field

--- polls/admin.py
+++ polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ['question_text', 'pub_date', 'was_published_recently']
     # list_filter = ('pub_date', 'question_text')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
     fieldsets = [
         (None, {
             'fields': ['question_text']
